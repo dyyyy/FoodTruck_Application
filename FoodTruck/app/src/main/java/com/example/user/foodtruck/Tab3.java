211836af9a6baaaae18d91be203e6e659302de53
dyyyy <dyj7857@gmail.com>
2018-03-01 18:00:43 +0900

    미완

---
+++ FoodTruck/app/src/main/java/com/example/user/foodtruck/Tab3.java
@@ -0,0 +1,21 @@
+package com.example.user.foodtruck;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by bit-user on 2018-03-01.
+ */
+
+public class Tab3 extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.menu_tab3, container, false);
+        return view;
+    }
+}
